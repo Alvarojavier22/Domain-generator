719bae0be7ec6f5d05ced7a8e93a15b8dcc5e870
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-11-04 15:14:02 +0000

    Empezando

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.getElementById;
+
+  function generarDominios() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+
+    let dominios = "";
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          let domino = pronoun[i] + adj[j] + noun[k] + ".com";
+          dominios =
+            dominios + '<li class ="list-group-item">' + dominios + "<li>";
+        }
+      }
+    }
+    document.getElementById("dominios").innerHTML = dominios;
+  }
 };
