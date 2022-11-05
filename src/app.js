39013385137944d71fa5da2cf86de2efc0bb2256
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-11-05 20:41:19 +0000

    funcionando

--- src/app.js
+++ src/app.js
@@ -5,31 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
+window.onload = function() {
   //write your code here
-  window.onload = function() {
-    console.log("Hello Rigo from the console!");
+  console.log("Hello Rigo from the console!");
 
-    document
-      .getElementById("generarDominios")
-      .addEventListener("click", generarDominios);
-    dominios();
-  };
-  function generarDominios() {
-    let pronoun = ["the", "our"];
-    let adj = ["great", "big"];
-    let noun = ["jogger", "racoon"];
+  document
+    .getElementById("generarDominios")
+    .addEventListener("click", generarDominios);
+  //.dominios();
+};
 
-    let dominios = "";
+function generarDominios() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
 
-    for (let i = 0; i < pronoun.length; i++) {
-      for (let j = 0; j < adj.length; j++) {
-        for (let k = 0; k < noun.length; k++) {
-          let domino = pronoun[i] + adj[j] + noun[k] + ".com";
-          dominios =
-            dominios + '<li class ="list-group-item">' + dominios + "<li>";
-        }
+  let dominios = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let dominio = pronoun[i] + adj[j] + noun[k] + ".com";
+        dominios =
+          dominios + '<li class ="list-group-item">' + dominio + "<li>";
       }
     }
   }
-};
+  document.getElementById("generarDominios").innerHTML = dominios;
+}
